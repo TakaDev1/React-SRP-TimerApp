4b84b57e35a969625ccaa6ee59985ae95fcb92d5
Izumi Takayuki <r8quattrospider@gmail.com>
2026-09-15 16:23:22 +0900

    feat: Implement CountTimer Reducer

---
+++ src/reducers/CountTimerReducer.ts
@@ -0,0 +1,20 @@
+import type { CountTimerAction, CountTimerType } from "../types/CountTimerType";
+
+const initialState: CountTimerType = { count: 0, isRunning: false };
+
+const CountTimerReducer = (state: CountTimerType, action: CountTimerAction): CountTimerType => {
+  switch (action.type) {
+    case "START":
+      return { ...state, isRunning: !state.isRunning };
+    case "STOP":
+      return { ...state, isRunning: false };
+    case "RESET":
+      return initialState;
+    case "TICK":
+      return { ...state, count: state.count + 1 };
+    default:
+      return state;
+  }
+};
+
+export { initialState, CountTimerReducer };
